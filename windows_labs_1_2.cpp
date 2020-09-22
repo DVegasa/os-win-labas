87cdc791b14b04bd5d149d3140274f7411d0ab6f
Ed Khalturin <DracVeg@gmail.com>
2020-09-22 13:33:52 +0400

    wip2

--- windows_labs_1_2.cpp
+++ windows_labs_1_2.cpp
@@ -7,54 +7,89 @@
 
 using namespace std;
 
-class ReadError : exception {};
+HANDLE openFile(char path[255], bool accessWrite = false) {
+    LPCTSTR lpFileName;                                 // file name
+    DWORD dwDesiredAccess = GENERIC_READ;               // access mode
+    DWORD dwShareMode = FILE_SHARE_READ;                // share mode
+    LPSECURITY_ATTRIBUTES lpSecurityAttributes = NULL;  // SD
+    DWORD dwCreationDisposition = OPEN_EXISTING;        // how to create
+    DWORD dwFlagsAndAttributes;                         // file attributes
+    HANDLE hTemplateFile;                               // handle to template file
 
-void askForData(string &pathFrom, string &pathTo, int &symbFrom) {
+    if (accessWrite) {
+        dwDesiredAccess |= GENERIC_WRITE;  // Разрешаем запись
+        dwShareMode = 0;                   // Включаем монопольный доступ
+    }
+
+    HANDLE handle = CreateFile(
+        LPCSTR(path),
+        dwDesiredAccess,
+        dwShareMode,
+        lpSecurityAttributes,
+        dwCreationDisposition,
+        dwFlagsAndAttributes,
+        NULL);
+
+    if (handle == INVALID_HANDLE_VALUE) {
+        stringstream ss;
+        ss << "OpenFileError: " << GetLastError();
+        throw ss.str();
+    }
+
+    return handle;
+}
+
+string _readFile(HANDLE handle, int symbFrom) {
+    return NULL;
+}
+
+void doCopy(char pathFrom[255], char pathTo[255], int symbFrom) {
+    HANDLE handleFrom;
+    HANDLE handleTo;
+
+    try {
+        handleFrom = openFile(pathFrom, false);
+        handleTo = openFile(pathTo, true);
+    } catch (string error) {
+        cout << error;
+    }
+
+    // string buffer = _readFile(handleFrom, symbFrom);
+}
+
+void deleteFile(char path[255]) {
+}
+
+int main(void) {
+    char pathFrom[255], pathTo[255];
+    int symbFrom;
+    bool toDelete;
+
+
     cout << "Введите адрес файла, откуда копировать:\n";
     cin >> pathFrom;
+
+    try {
+        HANDLE test = openFile(pathFrom, false);
+    } catch (string error) {
+        cout << error << endl;
+    }
+    cout << "OK" << endl;
+    return 0;
+
+
+
     cout << "Введите адрес файла, куда вставить:\n";
     cin >> pathTo;
     cout << "С какого символа начать копирование?:\n";
     cin >> symbFrom;
-}
+    cout << "Удалить после копирования?: \n";
+    cin >> toDelete;
 
-string _readFile(string pathFrom, int symbFrom) {
-    DWORD numberOfBytesRead;
+    doCopy(pathFrom, pathTo, symbFrom);
 
-    LPVOID lpBuffer;
-    DWORD nNumberOfBytesToRead;
-    LPDWORD lpNumberOfBytesRead = &numberOfBytesRead;
-
-    // Тут нужно открыть файл через CreateFile и получить HANDLE файла
-
-    HANDLE readResult = ReadFile(
-
-        GENERIC_READ,          // дескриптор файла
-        lpBuffer,              // буфер данных
-        nNumberOfBytesToRead,  // количество байт для прочтения
-        lpNumberOfBytesRead,   // количество фактически прочитанных байт
-        NULL);                 // указатель на структуру, используемую при асинхронном чтении
-
-    if (readResult == INVALID_HANDLE_VALUE) {
-        CloseHandle(readResult);
-        throw ReadError();
+    if (toDelete) {
+        deleteFile(pathFrom);
     }
-}
-
-void startCopying(string pathFrom, string pathTo, int symbFrom) {
-    try {
-        string buffer = _readFile(pathFrom, symbFrom);
-    } catch (ReadError readError) {
-        cout << "Ошибка чтения файла: " << GetLastError();
-        cout << endl;
-    }
-}
-
-int main(void) {
-    string pathFrom;
-    string pathTo;
-    int symbFrom;
-    askForData(pathFrom, pathTo, symbFrom);
-    startCopying(pathFrom, pathTo, symbFrom);
     return 0;
 }
