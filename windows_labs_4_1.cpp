4bda758dd56919cc572df8b543d9d44483832a62
Ed Khalturin <DracVeg@gmail.com>
2020-12-03 10:40:20 +0400

    WORKS

--- windows_labs_4_1.cpp
+++ windows_labs_4_1.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <iostream>
 #include <string>
 
@@ -5,81 +8,39 @@
 #include "windows.h"
 
 int main(void) {
-    // #region a
-    // LPCSTR lpApplicationName = NULL;
-    // LPSTR lpCommandLine = "calc.exe";
-    // LPSECURITY_ATTRIBUTES lpProcessAttributes = NULL;
-    // LPSECURITY_ATTRIBUTES lpThreadAttributes = NULL;
-    // BOOL bInheritHandles = FALSE;  // возможно нужно будет менять
-    // DWORD dwCreationFlags = 0;
-    // LPVOID lpEnvironment = NULL;
-    // LPCSTR lpCurrentDirectory = NULL;
-    // STARTUPINFOA si;
-    // PROCESS_INFORMATION pi;
-//
-    // ZeroMemory(&si, sizeof(si));
-    // si.cb = sizeof(si);
-    // ZeroMemory(&pi, sizeof(pi));
-//
-    // if (0 == CreateProcessA(
-    //              lpApplicationName,
-    //              lpCommandLine,
-    //              lpProcessAttributes,
-    //              lpThreadAttributes,
-    //              bInheritHandles,
-    //              dwCreationFlags,
-    //              lpEnvironment,
-    //              lpCurrentDirectory,
-    //              &si,
-    //              &pi)) {
-    //     error("CreateProcessA: " + GetLastError());
-    // }
-    // #endregion a
+    cout << "--- init ---" << endl;
 
-    // #region normal
-    // --------------------------
-    LPCTSTR lpApplicationName = NULL;
-    LPTSTR lpCommandLine = "calc.exe";
-    LPSECURITY_ATTRIBUTES lpProcessAttributes = NULL;
-    LPSECURITY_ATTRIBUTES lpThreadAttributes = NULL;
-    BOOL bInheritHandles = FALSE;
-    DWORD dwCreationFlags = 0;
-    LPVOID lpEnvironment = NULL;
-    LPCTSTR lpCurrentDirectory = NULL;
-    STARTUPINFO lpStartupInfo;
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&lpStartupInfo, sizeof(lpStartupInfo));
-    lpStartupInfo.cb = sizeof(lpStartupInfo);
-    ZeroMemory(&pi, sizeof(pi));
+    STARTUPINFO si = {0};
+    PROCESS_INFORMATION pi = {0};
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+
     if (0 == CreateProcess(
-        lpApplicationName,    // имя исполняемого модуля
-        lpCommandLine,        // Командная строка
-        lpProcessAttributes,  // Указатель на структуру SECURITY_ATTRIBUTES
-        lpThreadAttributes,   // Указатель на структуру SECURITY_ATTRIBUTES
-        bInheritHandles,      // Флаг наследования текущего процесса
-        dwCreationFlags,      // Флаги способов создания процесса
-        lpEnvironment,        // Указатель на блок среды
-        lpCurrentDirectory,   // Текущий диск или каталог
-        &lpStartupInfo,       // Указатель нас структуру STARTUPINFO
-        &pi                   // Указатель нас структуру PROCESS_INFORMATION
-    )) {
-         cout << "#Error: CreateProccess: " << GetLastError();
-         exit(0);
+                 NULL,        // имя исполняемого модуля
+                 "notepad.exe",  // Командная строка
+                 NULL,        // Указатель на структуру SECURITY_ATTRIBUTES
+                 NULL,        // Указатель на структуру SECURITY_ATTRIBUTES
+                 FALSE,       // Флаг наследования текущего процесса
+                 0,           // Флаги способов создания процесса
+                 0,           // Указатель на блок среды
+                 NULL,        // Текущий диск или каталог
+                 &si,         // Указатель нас структуру STARTUPINFO
+                 &pi          // Указатель нас структуру PROCESS_INFORMATION
+                 )) {
+        cout << "#Error: CreateProccess: " << GetLastError();
+        exit(0);
     }
-    // ------------------------
-    // #endregion normal
 
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    Sleep(100);
     cout << "Successfully created: dwProcessId=" << pi.dwProcessId << endl;
     cout << "Successfully created: dwThreadId=" << pi.dwThreadId << endl;
 
     while (true) {
-        print();
-        print("s: SuspendThread()");
-        print("r: ResumeThread()");
-        print("t: TerminateProcess");
-        print("Input command: ");
+        cout << endl;
+        cout << "s: SuspendThread()" << endl;
+        cout << "r: ResumeThread()" << endl;
+        cout << "t: TerminateProcess" << endl;
+        cout << "Input command: " << endl;
         char c;
         cin >> c;
 
@@ -87,15 +48,14 @@ int main(void) {
             if (0 == TerminateProcess(pi.hProcess, NO_ERROR)) {
                 cout << "#Error: TerminateProcess: " << GetLastError();
                 exit(0);
-            }
-            else {
+            } else {
                 CloseHandle(pi.hProcess);
                 CloseHandle(pi.hThread);
-                print("Terminated");
+                cout << "Terminated" << endl;
                 break;
             }
         }
     }
 
-    print("Global exit");
+    cout << "Global exit" << endl;
 }
